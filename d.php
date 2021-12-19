c547c1417d9cff54fc99d232707e8bd2b1486c3a
chat14 <96359514+chat14@users.noreply.github.com>
2021-12-19 07:55:25 +0700

    d.php

--- d.php
+++ d.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+// สำหรับใช้ในตัวอย่างการกำหนด session user_id
+if(isset($_POST['set_user1'])){
+    $_SESSION['ses_user_id']=$_POST['userID'];
+}
+if(isset($_POST['set_user2'])){
+    $_SESSION['ses_user_id2']=$_POST['userID'];
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
