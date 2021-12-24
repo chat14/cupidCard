cb4c075c209cdf347263f6e1c0d6b6dd91b7295c
chat14 <96359514+chat14@users.noreply.github.com>
2021-12-24 12:22:33 +0700

    index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ if(isset($_POST['set_user1'])){
 }
 if(isset($_POST['set_user2'])){
     $_SESSION['ses_user_id2']=$_POST['userID'];
+heroku pipelines:add example-pipeline -a example-staging-app
+? Stage of example-staging: staging
+Adding example-staging to example pipeline as staging... done
 }
 ?>
 <!DOCTYPE html>
